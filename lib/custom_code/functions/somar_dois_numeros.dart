cbb4d9bd785a685aa441ccaec6aff245361c4090
Pedro Borba <pedroborba@Pedros-MacBook-Air.local>
2025-07-18 14:43:59 -0300

    feat: Adicionada Custom Function somarDoisNumeros e atualizadas dependências

---
+++ lib/custom_code/functions/somar_dois_numeros.dart
@@ -0,0 +1,11 @@
+// lib/custom_code/functions/somar_dois_numeros.dart
+import 'package:flutter/material.dart'; // Import padrão do FlutterFlow para Custom Code
+
+// Nome da função: somarDoisNumeros
+// Tipo de retorno: int
+// Argumentos: int a, int b
+int somarDoisNumeros(int a, int b) {
+  /// Esta função foi criada DIRETAMENTE no VS Code!
+  /// E ela soma dois números inteiros.
+  return a + b;
+}
